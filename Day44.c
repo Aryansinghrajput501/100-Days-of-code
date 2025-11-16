5c85c39e607f305a983cefea657fff81df6b0c32
Aryansinghrajput501 <aryansingh05548@gmail.com>
2025-11-16 15:48:59 +0530

    Add string manipulation functions for counting and replacing

---
+++ Day44.c
@@ -0,0 +1,53 @@
+
+/*Q87 (Strings)
+Count spaces, digits, and special characters in a string. */
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+    
+    char str[100];
+    int i = 0;
+    printf("Enter any string: \n");
+    fgets(str,sizeof(str),stdin);
+    int spaces = -1, digits = 0, spchar = 0;
+    while (str[i] != '\0'){
+        if (isdigit(str[i])){
+            digits++;
+        }
+        else if (isspace(str[i])){
+            spaces++;
+        }
+        else if (!isalpha(str[i])){
+            spchar++;
+        }
+        i++;
+    }
+
+    printf("Number of spaces: %d\nNumber of digits: %d\nNumber of special characters: %d\n", spaces,digits,spchar);
+    return 0;
+}
+
+
+/*Q88 (Strings)
+Replace spaces with hyphens in a string. */
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+    
+    char str[100];
+    int i = 0;
+    printf("Enter any string: \n");
+    fgets(str,sizeof(str),stdin);
+
+    while (str[i] != '\0'){
+        if (str[i] == ' '){
+            str[i] = '-';
+        }
+    i++;
+    }
+    printf("%s", str);
+}
