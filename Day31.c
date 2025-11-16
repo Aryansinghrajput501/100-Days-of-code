1f3758c47da0fa11517f6500d1edd11048c09ee3
Aryansinghrajput501 <aryansingh05548@gmail.com>
2025-11-16 15:36:25 +0530

    Add linear search and array reversal functions
    Implemented linear search and array reversal in C.

---
+++ Day31.c
@@ -0,0 +1,67 @@
+
+/*Q61 (Arrays (1D))
+Search for an element in an array using linear search. */
+
+#include <stdio.h>
+
+int main(){
+    int len,find;
+    printf("Enter the length of array: \n");
+    scanf("%d", &len);
+    int arr[len], flag =0;
+
+    for (int i=0; i<len; i++){
+        printf("Enter element %d: ",i+1);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element you want to find: ");
+    scanf("%d", &find);
+    
+    for (int i=0; i<len; i++){
+        if (arr[i] == find){
+            printf("Found at index %d.", i);
+            flag = 1;
+            break;
+        }
+        else {
+            continue;
+        }
+    }
+
+    if (flag == 0){
+        printf("Not Found.");
+    }
+}
+
+
+/*Q62 (Arrays (1D))
+Reverse an array without taking extra space. */
+
+#include <stdio.h>
+
+int main(){
+    int len;
+    printf("Enter the length of array: \n");
+    scanf("%d", &len);
+    int arr[len], flag =0;
+
+    for (int i=0; i<len; i++){
+        printf("Enter element %d: ",i+1);
+        scanf("%d", &arr[i]);
+    }
+
+    int a = 0, b = len - 1;
+        while (a < b) {
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+
+            a++;
+            b--;
+    }
+
+    for (int i=0; i<len; i++){
+        printf("%d ", arr[i]);
+    }
+}
